5c65182235d79567d32f1e6f0843e1e66310f065
1292516006 <felipe.mouramoreira@gmail.com>
2025-05-30 20:48:03 -0300

    feat: tela principal e menu

--- src/main/java/GerenciadorProjetosApp.java
+++ src/main/java/GerenciadorProjetosApp.java
@@ -1,21 +1,51 @@
-package models;
-
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class GerenciadorProjetosApp {
-    private static EntityManagerFactory emf;
+import javax.swing.*;
+import java.awt.*;
+
+public class GerenciadorProjetosApp extends JFrame {
+    private CardLayout cardLayout;
+    private JPanel mainPanel;
+
+    private final String EMPTY_SCREEN = "EMPTY_SCREEN";
+    public GerenciadorProjetosApp(){
+        setTitle("Gerenciador de Projetos");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        this.cardLayout = new CardLayout();
+        this.mainPanel = new JPanel(cardLayout);
+
+        JPanel emptyPanel = new JPanel(new BorderLayout());
+        emptyPanel.add(new JLabel("Bem-vindo! Use o menu para navegar",
+                                         SwingConstants.CENTER),
+                         BorderLayout.CENTER);
+
+        this.mainPanel.add(emptyPanel, EMPTY_SCREEN);
+
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Menu");
+
+        JMenuItem listUserMenu = new JMenuItem("Lista Usuários");
+        JMenuItem exitItem = new JMenuItem("Sair");
+
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+        menu.add(listUserMenu);
+        menu.add(exitItem);
+
+        add(mainPanel);
+
+    }
 
     public static void main(String[] args) {
-        emf = Persistence.createEntityManagerFactory("PU");
-        EntityManager em = emf.createEntityManager();
+        SwingUtilities.invokeLater(() -> {
+            new GerenciadorProjetosApp().setVisible(true);
+        });
 
-        Usuario usuario =
-                new Usuario(1, "Muttley", "muttley@gmail.com",
-                        "12345");
-        em.getTransaction().begin();
-        em.persist(usuario);
-        em.getTransaction().commit();
     }
 }
